6d5dcdd8c3ca6e1a3fcfa8982f31881e3bff681d
Matthew The <matthew.the@tum.de>
2023-10-24 22:25:54 +0200

    Add empty lines between paragraphs

--- main.py
+++ main.py
@@ -12,30 +12,10 @@ def main():
 
     if st.button("Remove Articles and Fill In"):
         if user_input_text:
-            # Split the text into words
-            words = user_input_text.split()
-
-            # Initialize a list to store user input for articles
-            user_inputs = []
-
             st.subheader("Fill in the Removed Articles:")
-
-            # Use HTML and CSS to create an inline layout
-            inline_layout = '<div style="display: flex; flex-wrap: wrap;">'
-
-            i = 0
-            for word in words:
-                if word.lower() in german_articles:
-                    user_input = f'<input type="text" alt="{word}" style="width: 50px; height: 22px;" class="artikel_input">'
-                    # st.text_input(f"Fill in the article for '{word}':", key=f"input_{i}")
-                    i += 1
-                    inline_layout += f'<div style="margin-right: 5px;">{user_input}</div>'
-                else:
-                    inline_layout += f'<div style="margin-right: 5px;">{word}</div>'
-
-            inline_layout += '</div>'
-             
-            st.markdown(inline_layout, unsafe_allow_html=True)
+            
+            for sentence in user_input_text.splitlines():
+                create_text_with_holes(sentence)
             
             js = '''<script>
             let elements = window.parent.document.querySelectorAll('.artikel_input');
@@ -53,6 +33,38 @@ def main():
             </script>
             '''
             components.html(js)
+
+
+def create_text_with_holes(sentence: str):
+    # Split the text into words
+    words = sentence.split()
+
+    # Initialize a list to store user input for articles
+    user_inputs = []
+    
+
+    # Use HTML and CSS to create an inline layout
+    inline_layout = '<div style="display: flex; flex-wrap: wrap;">'
+
+    i = 0
+    for word in words:
+        if word.lower() in german_articles:
+            user_input = f'<input type="text" alt="{word}" style="width: 50px; height: 22px;" class="artikel_input">'
+            # st.text_input(f"Fill in the article for '{word}':", key=f"input_{i}")
+            i += 1
+            inline_layout += f'<div style="margin-right: 5px;">{user_input}</div>'
+        else:
+            inline_layout += f'<div style="margin-right: 5px;">{word}</div>'
+
+    inline_layout += '</div>'
+     
+    st.markdown(inline_layout, unsafe_allow_html=True)
+    
+    
+
+
+
+
 if __name__ == "__main__":
     main()
 
