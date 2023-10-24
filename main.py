92496a88d81f71394d74c219d7ba3f0466cd3d95
Matthew The <matthew.the@tum.de>
2023-10-24 23:36:43 +0200

    Add SZ articles as input

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+import feedparser
+import requests
+from bs4 import BeautifulSoup
+
+
 # Define a list of German articles
 german_articles = ["der", "die", "das", "den", "dem", "des"]
 
+
 def main():
+    st.set_page_config(layout="wide")
+    
     st.title("German Article Trainer")
+    
+    if 'submit' not in st.session_state:
+        st.session_state.submit = False
+    
+    col1, col2 = st.columns(2)
 
-    # Text input field for the user to paste their text
-    user_input_text = st.text_area("Paste your German text here:")
+    with col1:
+        # Text input field for the user to paste their text
+        user_input_text = st.text_area("Paste your German text here:", key="input")
+        
+        st.button("Remove Articles and Fill In", on_click=click_submit)
+        if st.session_state.submit:
+            if user_input_text:
+                create_form_with_holes(user_input_text)
 
-    if st.button("Remove Articles and Fill In"):
-        if user_input_text:
-            st.subheader("Fill in the Removed Articles:")
-            
-            for sentence in user_input_text.splitlines():
-                create_text_with_holes(sentence)
-            
-            js = '''<script>
-            let elements = window.parent.document.querySelectorAll('.artikel_input');
 
-            elements.forEach((item) => {
-                item.addEventListener("change", () => {
-                    answerKey = item.alt
-                    if (item.value.trim().toLowerCase() === answerKey.toLowerCase()) {
-                        item.style.color = "green";
-                    } else {
-                        item.style.color = "red";
-                    }
-                })
-            });
-            </script>
-            '''
-            components.html(js)
+    with col2:
+        st.subheader("Train with articles from SZ.de")
+        get_article_links()
+
+
+def click_submit():
+    st.session_state.submit = True
+
+
+def create_form_with_holes(user_input_text):
+    st.subheader("Fill in the Removed Articles:")
+    
+    for sentence in user_input_text.splitlines():
+        create_text_with_holes(sentence)
+    
+    js = '''<script>
+    let elements = window.parent.document.querySelectorAll('.artikel_input');
+
+    elements.forEach((item) => {
+        item.addEventListener("change", () => {
+            answerKey = item.alt
+            if (item.value.trim().toLowerCase() === answerKey.toLowerCase()) {
+                item.style.color = "green";
+            } else {
+                item.style.color = "red";
+            }
+        })
+    });
+    </script>
+    '''
+    components.html(js)
+
+
+def get_article_links(top_n: int = 10):
+    url = "https://rss.sueddeutsche.de/rss/Topthemen"
+    feed = feedparser.parse(url)
+    
+    for entry in feed.entries[:top_n]:
+        st.button(entry.title, on_click=update_input_text, args=[entry.link])
+
+
+def update_input_text(url):
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    body = soup.find(itemprop="articleBody")
+    
+    article_text = ""
+    for paragraph in body.find_all("p", attrs={'data-manual': 'paragraph'}):
+        article_text += paragraph.get_text() + "\n"
+        
+    st.session_state.input = article_text
+    click_submit()
 
 
 def create_text_with_holes(sentence: str):
